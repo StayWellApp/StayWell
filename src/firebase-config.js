d1715c6a42c64c3d6876157f84fcac144805a5a2
ncam11 <neilcamilleri11@gmail.com>
2025-07-12 14:09:11 +0000

    Jul 12, 2025, 4:09 PM

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCRvsod68MdgMagGx4SKWpK57qM7N7qyZ8",
   authDomain: "staywellapp-49b62.firebaseapp.com",
   projectId: "staywellapp-49b62",
-  storageBucket: "staywellapp-49b62.appspot.com",
+  storageBucket: "staywellapp-49b62.firebasestorage.app",
   messagingSenderId: "240146050261",
   appId: "1:240146050261:web:9b807607032c45a452465c",
   measurementId: "G-N2YJYST032"
