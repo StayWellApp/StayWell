e10720851ff3f15246c89c98f4e367dca078e5d5
ncam11 <neilcamilleri11@gmail.com>
2025-09-27 22:33:07 +0000

    Sep 28, 2025, 12:33 AM

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,29 @@
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+// Import all functions from their modules
 const activityLogs = require('./src/activity-logs');
 const automation = require('./src/automation');
 const adminTasks = require('./src/admin');
 const tasks = require('./src/tasks');
 
-exports.activity = activityLogs;
-exports.automation = automation;
-exports.admin = adminTasks;
-exports.tasks = tasks;
+// Export each function individually at the top level
+// This makes them discoverable by Firebase
+exports.logUserCreation = activityLogs.logUserCreation;
+exports.logUserUpdates = activityLogs.logUserUpdates;
+exports.logPropertyCreation = activityLogs.logPropertyCreation;
+exports.logPropertyUpdates = activityLogs.logPropertyUpdates;
+
+exports.uploadProof = automation.uploadProof;
+exports.onBookingReceived = automation.onBookingReceived;
+exports.syncIcalFeeds = automation.syncIcalFeeds;
+exports.addManualBooking = automation.addManualBooking;
+
+exports.createImpersonationToken = adminTasks.createImpersonationToken;
+exports.logAdminAction = adminTasks.logAdminAction;
+exports.createClient = adminTasks.createClient;
+exports.createReauthenticationToken = adminTasks.createReauthenticationToken;
+
+exports.respondToTaskOffer = tasks.respondToTaskOffer;
+exports.submitForInspection = tasks.submitForInspection;
+exports.reviewTask = tasks.reviewTask;
