e57f705cb971118e80182cdcdd75f6b0684013eb
ncam11 <neilcamilleri11@gmail.com>
2025-11-01 19:35:25 +0000

    Nov 1, 2025, 8:35 PM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './index.css';
 import './i18n';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './contexts/ThemeContext';
