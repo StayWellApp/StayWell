01997696cb67f8e75eea0e1c1e33d8705865cba0
ncam11 <neilcamilleri11@gmail.com>
2025-11-01 20:01:16 +0000

    Nov 1, 2025, 9:01 PM

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import './i18n';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // <-- Imports the CSS you just fixed
+import './index.css'; // <-- FIX #1: Imports the CSS file
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './components/Auth'; // <-- 1. Imports Auth logic
-import { AdminProvider } from './contexts/AdminContext'; // <-- 2. Imports Admin logic
+import { AuthProvider } from './components/Auth'; // <-- FIX #2: Imports Auth logic
+import { AdminProvider } from './contexts/AdminContext'; // <-- FIX #3: Imports Admin logic
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <AuthProvider> {/* <-- 3. Provides Auth logic to App */}
-          <AdminProvider> {/* <-- 4. Provides Admin logic to App */}
+        <AuthProvider> {/* <-- FIX #2: Provides Auth logic to App */}
+          <AdminProvider> {/* <-- FIX #3: Provides Admin logic to App */}
             <App />
           </AdminProvider>
         </AuthProvider>
