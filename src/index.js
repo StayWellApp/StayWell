510b41706c844c95ed8920cb3ae6bda8f452b935
ncam11 <neilcamilleri11@gmail.com>
2025-11-01 19:54:37 +0000

    Nov 1, 2025, 8:54 PM

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import './i18n';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // This correctly imports your styles
+import './index.css'; // <-- FIX #1: This imports all your styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './components/Auth'; // <-- 1. Import AuthProvider
-import { AdminProvider } from './contexts/AdminContext'; // <-- 2. Import AdminProvider
+import { AuthProvider } from './components/Auth'; // <-- FIX #2: Import AuthProvider
+import { AdminProvider } from './contexts/AdminContext'; // <-- FIX #3: Import AdminProvider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <AuthProvider> {/* <-- 3. Wrap App in AuthProvider */}
-          <AdminProvider> {/* <-- 4. Wrap App in AdminProvider */}
+        <AuthProvider> {/* <-- FIX #2: Wrap App in AuthProvider */}
+          <AdminProvider> {/* <-- FIX #3: Wrap App in AdminProvider */}
             <App />
           </AdminProvider>
         </AuthProvider>
